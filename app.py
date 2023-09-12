66c6b72d2c751ecdbb6aa947f8d2cc9c35d2c358
OKUDAYUKI08 <yukidorakue2828@gmail.com>
2023-09-12 18:36:10 +0900

    ページ遷移の作成

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def  index():
+    return render_template("index.html")
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+@app.route("/show")
+def show():
+    return render_template("show.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
