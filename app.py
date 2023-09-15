e2bf5b152e1cc42c683bd6926c8c95c026c4d831
OKUDAYUKI08 <yukidorakue2828@gmail.com>
2023-09-15 16:24:17 +0900

    情報の受け渡しができました

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app=Flask(__name__)
 
@@ -9,13 +9,14 @@ def  index():
 @app.route("/new")
 def new():
     return render_template("new.html")
-@app.route("/show")
+@app.route("/show",methods=['GET','POST'])
 def show():
-    return render_template("show.html")
+    if request.method=='GET':
+        return render_template("show.html")
+    if request.method=='POST':
+        result=request.form['data1']
+        return render_template("show.html",result=result)
 
-@app.route("/show-input")
-def show():
-    return render_template("show.html",)
 
 if __name__=="__main__":
     app.run(debug=True)
